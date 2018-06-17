302aa4e0689bee81bba41b357a9648ddd4f01a16
Tatloani <33587387+Tatloani@users.noreply.github.com>
2018-06-16 22:21:09 -0700

    Basic code files

---
+++ iconbinary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+[Serializable]
+class Icons
+{
+    private Icon[] list;
+
+    public Icons(int i)
+    {
+        if (i < 1)
+            throw new Exception("Value most be positive");
+        list = new Icon[i];
+    }
+
+    public Icon this[int index]
+    {
+        get
+        {
+            if (index < 0 || index > list.Length)
+                throw new IndexOutOfRangeException("Out of Range Value");
+            else return list[index];
+        }
+        set
+        {
+            if (index < 0 || index > list.Length)
+                throw new IndexOutOfRangeException("Out of Range Value");
+            else list[index] = value;
+        }
+    }
+}
