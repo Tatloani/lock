7588ca23d9f5372d449e2b0f0864cf25795a4b7c
Tatloani <33587387+Tatloani@users.noreply.github.com>
2018-06-16 22:24:35 -0700

    Update main.cs

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
 using System.Windows.Forms;
 
-public class Daemon
+public class Lock
 {
     static NotifyIcon Ico;
     static Icons iconos;
